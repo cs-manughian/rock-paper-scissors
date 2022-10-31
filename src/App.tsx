db9b5c8e8461b81b9ee3bac8013b70902919e79c
Cosima Peter <cs.manughian@gmail.com>
2022-10-31 13:41:53 -0700

    Scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PlayerName } from './components/PlayerName';
 import { Row, Col } from './components/Layout';
 import { FadeIn } from './components/FadeIn';
 import { capitalize } from './util';
+import { ScoreBoard } from './components/ScoreBoard';
 
 const WinningPairs = {
   [Hands.Scissors]: Hands.Paper,
@@ -13,45 +14,57 @@ const WinningPairs = {
   [Hands.Paper]: Hands.Rock,
 }
 
+type Scores = {
+  player: number,
+  computer: number,
+}
+
 function App() {
 
   const [playerName, setPlayerName] = useState<string>('');
   const [winnerText, setWinnerText] = useState<string>('');
+  const [scores, setScores] = useState<Scores>({ player: 0, computer: 0 });
 
   const [_, setPlayerHand] = useState<Hands>();
   const setHand = useCallback(
     (hand: Hands) => {
       setWinnerText('');
       setPlayerHand(hand);
-      calculateWinner(hand);
-      saveScores();
+      const newScores = calculateWinner(hand);
+      setScores({ ...newScores });
     }
-  , []);
+  , [scores]);
 
-  function calculateWinner(playerHand: Hands) {
+  function calculateWinner(playerHand: Hands) : Scores {
     const NUM_OUTCOMES = 3;
     const computer = Math.floor(Math.random() * NUM_OUTCOMES);
     const computersHand = HandsNumeric[computer] as Hands;
 
+    let newScores: Scores = { ...scores };
+
     if (WinningPairs[playerHand] === computersHand) {
       setWinnerText(`You win! ${capitalize(playerHand)} beats ${capitalize(computersHand)}.`);
+      newScores.player = scores.player + 1;
 
     } else if (WinningPairs[computersHand] === playerHand) {
       setWinnerText(`Computer wins! ${capitalize(computersHand)} beats ${capitalize(playerHand)}.`);
+      newScores.computer = scores.computer + 1;
 
     } else {
       setWinnerText(`It's a draw! ${capitalize(playerHand)} versus ${capitalize(computersHand)}.`);
     }
 
-  }
-
-  function saveScores() {
-
+    return newScores;
   }
 
   return (
     <div className='app'>
       <div className='game'>
+        <ScoreBoard 
+          playerName={playerName} 
+          playerScore={scores.player}
+          computerScore={scores.computer}
+        />
         <Row>
           <h1>Rock, Paper, Scissors</h1>
           <PlayerName setPlayerName={(name) => setPlayerName(name)} />
--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,2 +1,37 @@
 
-export {}
+import { Col, Row } from './Layout';
+import './ScoreBoard.css';
+
+type ScoreBoardProps = {
+    playerName: string,
+    playerScore: number,
+    computerScore: number
+}
+
+export const ScoreBoard: React.FC<ScoreBoardProps> = ({
+    playerName,
+    playerScore,
+    computerScore
+}) => {
+    return (
+        <div className='scoreboard'>
+            {
+                playerName && (
+                <>
+                    <h1>Scores</h1>
+                    <Row>
+                        <Col>
+                            <b>{`${playerName}`}</b>
+                            <span>{`${playerScore}`}</span>
+                        </Col>
+                        <Col>
+                            <b>Computer</b>
+                            <span>{`${computerScore}`}</span>
+                        </Col>
+                    </Row>
+                </>
+                )
+            }
+        </div>
+    )
+}
